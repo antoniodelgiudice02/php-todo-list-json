fcf42dbf3125d518a9186a59a877bb987ef4e851
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-02-29 22:45:36 +0100

    add axios.post

--- frontend/main.js
+++ frontend/main.js
@@ -5,7 +5,9 @@ const app = createApp({
     return {
       title: "Todo List",
 
-      tasks: []
+      tasks: [],
+
+      newTask: '',
     };
   },
 
@@ -14,7 +16,23 @@ const app = createApp({
       axios.get('http://localhost/php-todo-list-json/backend/API/get-list.php').then((response) => {console.log(response.data)
       this.tasks = response.data;
     })
-      
+    },
+
+    addTask(){
+
+      const task = this.newTask
+
+      this.newTask = ''
+
+      const data = { task }
+
+      const header = {
+        headers: {'Content-Type': 'multipart/form-data'}
+      }
+
+
+
+      axios.post('http://localhost/php-todo-list-json/backend/API/add-task.php', data, header).then((response) => {console.log(response.data)})
     }
   },
 
