34a65cbf651f3a268302caaea1197905497ca297
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-02-29 17:43:54 +0100

    add list

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,34 @@
 const { createApp } = Vue;
 
 const app = createApp({
-    data(){
-        return{
-            title: 'Todo List'
-        }
-    }
-})
+  data() {
+    return {
+      title: "Todo List",
 
-app.mount('#app')
+      tasks: [
+        {
+          text: "task 1",
+          done: true,
+        },
+        {
+          text: "task 2",
+          done: false,
+        },
+        {
+          text: "task 3",
+          done: true,
+        },
+        {
+          text: "task 4",
+          done: false,
+        },
+        {
+          text: "task 5",
+          done: true,
+        },
+      ],
+    };
+  },
+});
+
+app.mount("#app");
